b9be493fde2d9f818b1c01bcd1afd6111a8e330e
GuyBlumenthal <guy.blumenthal@outlook.com>
2024-11-13 12:45:18 -0500

    Fixed errors

--- decisions.py
+++ decisions.py
@@ -18,57 +18,57 @@ from controller import controller, trajectoryController
 
 
 class decision_maker(Node):
-    
-    
+
+
     def __init__(self, publisher_msg, publishing_topic, qos_publisher, rate=10, motion_type=POINT_PLANNER):
 
         super().__init__("decision_maker")
 
         self.publisher=self.create_publisher(publisher_msg, publishing_topic, qos_profile=qos_publisher)
-        
-        
+
+
         publishing_period=1/rate
 
         self.reachThreshold=0.01
 
 
         # TODO Part 3: use the Kalman Filter
-        self.localizer=localization(...)
-        
+        self.localizer=localization(kalmanFilter, publishing_period)
+
         if motion_type==POINT_PLANNER:
-            self.controller=controller(klp=0.2, klv=0.5, kap=0.8, kav=0.6)      
+            self.controller=controller(klp=0.2, klv=0.5, kap=0.8, kav=0.6)
             self.planner=planner(POINT_PLANNER)
 
-        
+
         elif motion_type==TRAJECTORY_PLANNER:
-            self.controller=trajectoryController(klp=0.2, klv=0.5, kap=0.8, kav=0.6)      
+            self.controller=trajectoryController(klp=0.2, klv=0.5, kap=0.8, kav=0.6)
             self.planner=planner(TRAJECTORY_PLANNER)
         elif motion_type==SPIRAL_4TUNE:
             self.controller=None
             self.planner=None
             self.linearVelocity = 0
-        
+
         else:
             print("Error! you don't have this type of planner", file=sys.stderr)
             return
 
         self.motion_type=motion_type
         self.goal=self.planner.plan() if self.motion_type != SPIRAL_4TUNE else None
-        
+
 
         self.create_timer(publishing_period, self.timerCallback)
 
 
 
     def timerCallback(self):
-        
+
         spin_once(self.localizer)
 
         if self.localizer.getPose() is  None:
             print("waiting for odom msgs ....")
             return
-        
-        
+
+
         vel_msg=Twist()
 
         if self.motion_type == SPIRAL_4TUNE:
@@ -78,39 +78,39 @@ class decision_maker(Node):
             self.publisher.publish(vel_msg)
             return
 
-        
+
         if type(self.goal) == list:
             reached_goal=True if calculate_linear_error(self.localizer.getPose(), self.goal[-1]) <self.reachThreshold else False
-        else: 
+        else:
             reached_goal=True if calculate_linear_error(self.localizer.getPose(), self.goal) <self.reachThreshold else False
 
 
         if reached_goal:
             print("reached goal")
             self.publisher.publish(vel_msg)
-            
+
             self.controller.PID_angular.logger.save_log()
             self.controller.PID_linear.logger.save_log()
-            
+
             raise SystemExit
-        
+
         velocity, yaw_rate = self.controller.\
             vel_request(self.localizer.getPose(), self.goal, True)
 
-        
+
         vel_msg.linear.x=velocity
         vel_msg.angular.z=yaw_rate
-        
+
         self.publisher.publish(vel_msg)
 
 
 import argparse
 def main(args=None):
-    
-    
+
+
     init()
-    
-    
+
+
     if args.motion == "point":
         DM=decision_maker(Twist, "/cmd_vel", 10, motion_type=POINT_PLANNER)
     elif args.motion == "trajectory":
@@ -132,8 +132,8 @@ def main(args=None):
 
 
 if __name__=="__main__":
-    
-    argParser=argparse.ArgumentParser(description="point or trajectory") 
+
+    argParser=argparse.ArgumentParser(description="point or trajectory")
     argParser.add_argument("--motion", type=str, default="spiral")
     args = argParser.parse_args()
 
--- localization.py
+++ localization.py
@@ -24,58 +24,58 @@ kalmanFilter=1
 odom_qos=QoSProfile(reliability=2, durability=2, history=1, depth=10)
 
 class localization(Node):
-    
+
     def __init__(self, type, dt, loggerName="robotPose.csv", loggerHeaders=["imu_ax", "imu_ay", "kf_ax", "kf_ay","kf_vx","kf_w","kf_x", "kf_y","stamp"]):
 
         super().__init__("localizer")
 
-        elf.loc_logger=Logger( loggerName , loggerHeaders)
+        self.loc_logger=Logger( loggerName , loggerHeaders)
         self.pose=None
-        
+
         if type==rawSensors:
             self.initRawSensors()
         elif type==kalmanFilter:
             self.initKalmanfilter(dt)
         else:
             print("We don't have this type for localization", sys.stderr)
-            return  
+            return
 
     def initRawSensors(self):
         self.create_subscription(odom, "/odom", self.odom_callback, qos_profile=odom_qos)
-        
+
     def initKalmanfilter(self, dt):
-        
+
         # TODO Part 3: Set up the quantities for the EKF (hint: you will need the functions for the states and measurements)
-        
+
         x= ...
-        
+
         Q= ...
 
         R= ...
-        
+
         P= ... # initial covariance
-        
+
         self.kf=kalman_filter(P,Q,R, x, dt)
-        
+
         # TODO Part 3: Use the odometry and IMU data for the EKF
         self.odom_sub=message_filters.Subscriber(...)
         self.imu_sub=message_filters.Subscriber(...)
-        
+
         time_syncher=message_filters.ApproximateTimeSynchronizer([self.odom_sub, self.imu_sub], queue_size=10, slop=0.1)
         time_syncher.registerCallback(self.fusion_callback)
-    
+
     def fusion_callback(self, odom_msg: odom, imu_msg: Imu):
-        
+
         # TODO Part 3: Use the EKF to perform state estimation
         # Take the measurements
         # your measurements are the linear velocity and angular velocity from odom msg
         # and linear acceleration in x and y from the imu msg
         # the kalman filter should do a proper integration to provide x,y and filter ax,ay
         z=...
-        
+
         # Implement the two steps for estimation
         ...
-        
+
         # Get the estimate
         xhat=self.kf.get_states()
 
@@ -84,9 +84,9 @@ class localization(Node):
 
         # TODO Part 4: log your data
         self.loc_logger.log_values(...)
-      
+
     def odom_callback(self, pose_msg):
-        
+
         self.pose=[ pose_msg.pose.pose.position.x,
                     pose_msg.pose.pose.position.y,
                     euler_from_quaternion(pose_msg.pose.pose.orientation),
@@ -98,9 +98,9 @@ class localization(Node):
 
 
 if __name__=="__main__":
-    
+
     init()
-    
+
     LOCALIZER=localization()
-    
+
     spin(LOCALIZER)
