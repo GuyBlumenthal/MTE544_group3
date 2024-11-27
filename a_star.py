a1bc36639217f68846573c3bad0c03463a17625f
Christopher Chung <tps90067@gmail.com>
2024-11-27 16:05:40 -0500

    leNewMove

--- a_star.py
+++ a_star.py
@@ -111,14 +111,14 @@ def search(maze, start, end):
 
     # TODO PART 4 what squares do we search . serarch movement is left-right-top-bottom
     # (4 or 8 movements) from every positon
-    move = [[...],  # go up
-            [...],  # go left
-            [...],  # go down
-            [...],  # go right
-            [...],  # go up left
-            [...],  # go down left
-            [...],  # go up right
-            [...]]  # go down right
+    move = [[0,1],  # go up
+            [-1,0],  # go left
+            [0,-1],  # go down
+            [1,0],  # go right
+            [-1,1],  # go up left
+            [-1,-1],  # go down left
+            [1,1],  # go up right
+            [1,0]]  # go down right
 
     """
         1) We first get the current node by comparing all f cost and selecting the lowest cost node for further expansion
@@ -176,7 +176,7 @@ def search(maze, start, end):
         for new_position in move:
 
             # TODO PART 4 Get node position
-            node_position = (...)
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # TODO PART 4 Make sure within range (check if within maze boundary)
             if (...):
